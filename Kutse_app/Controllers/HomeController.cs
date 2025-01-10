406db6a318ae2f4c0aa375b43139ec3d3dbcba4f
GlebSotsjov <glebsotjov@gmail.com>
2025-01-10 11:02:08 +0200

    fix

--- Kutse_app/Controllers/HomeController.cs
+++ Kutse_app/Controllers/HomeController.cs
@@ -1,10 +1,7 @@
 ﻿using Kutse_app.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Web.Helpers;
 
 namespace Kutse_app.Controllers
 {
@@ -17,6 +14,7 @@ namespace Kutse_app.Controllers
             ViewBag.Greeting = hour < 10 ? "Tere hommikust!" : "Tere päevast!";
             return View();
         }
+
         [HttpGet]
         public ViewResult Ankeet()
         {
@@ -36,24 +34,53 @@ namespace Kutse_app.Controllers
                 return View();
             }
         }
+
         public void E_Mail(Guest guest)
         {
             try
             {
+                // Замените "your-public-url.com" на ваш публичный адрес
+                string baseUrl = "https://your-public-url.com";
+                string yesLink = $"{baseUrl}/Home/Answer?guestName={guest.Name}&answer=true";
+                string noLink = $"{baseUrl}/Home/Answer?guestName={guest.Name}&answer=false";
+
+                string body = $"<html><body>" +
+                              $"<p>Здравствуйте, {guest.Name}!</p>" +
+                              $"<p>Приглашаем вас {ViewBag.Message}</p>" +
+                              $"<p><a href='{yesLink}'><button>Да</button></a> " +
+                              $"<a href='{noLink}'><button>Нет</button></a></p>" +
+                              $"</body></html>";
+
                 WebMail.SmtpServer = "smtp.gmail.com";
                 WebMail.SmtpPort = 587;
                 WebMail.EnableSsl = true;
                 WebMail.UserName = "glebsotjov@gmail.com";
-                WebMail.Password = "dikf aipr rebq ytsk";
+                WebMail.Password = "dikf aipr rebq ytsk"; // Замените на ваш пароль
                 WebMail.From = "glebsotjov@gmail.com";
-                WebMail.Send("glebsotjov@gmail.com", "Vastus kutsele", guest.Name + " Vastus " + ((guest.WillAttend ?? false) ?
-                    "tuleb poele " : "ei tule poele"));
+                WebMail.Send("marina.oleinik@tthk.ee", "Vastus kutsele", body, isBodyHtml: true);
+
                 ViewBag.Message = "Kiri on saatnud!";
             }
             catch (Exception ex)
             {
                 ViewBag.Message = "Mul on kuhju! Ei saa kirja saada!!!";
             }
+        }
+
+        public ActionResult Answer(string guestName, bool answer)
+        {
+            try
+            {
+                string answerText = answer ? "tuleb poele" : "ei tule peole";
+                WebMail.Send("glebsotjov@gmail.com", "Ответ на приглашение", $"{guestName} {answerText}");
+            }
+            catch (Exception ex)
+            {
+                // Обработка ошибки отправки, например, запись в лог
+                return View("Error"); //  Можно создать представление для отображения ошибки
+            }
+
+            return RedirectToAction("Thanks"); // Перенаправление на страницу Thanks
         }
     }
 }
